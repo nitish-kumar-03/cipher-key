ebc7203bc2ba72510853d8c97285d6bf5dd180e0
Nitish Kumar <nk9990207902@gmail.com>
2025-03-09 13:28:00 +0530

    server files & folders setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,38 @@
 // imports
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// calls
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// variables
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+const MONGO_URL = process.env.MONGO_URL;
+
+if (!PORT || !HOST || !MONGO_URL) {
+  console.error("❌ env variables are missing");
+}
+
+// route
+app.get("/", (req, res) => {
+  res.send("<h1>👋 hello from server");
+});
+
+// db
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log(`📦 database : connected`);
+    app.listen(PORT, () => {
+      console.log(`🚀 server : http://${HOST}:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
